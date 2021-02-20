3741d7a3436104e88da16579a7b77c56868cfdba
phdafoe <phdafoe@gmail.com>
2021-02-20 09:44:28 +0100

    Initial Commit

---
+++ TabBarShowToggle/TabBarShowToggleApp.swift
@@ -0,0 +1,17 @@
+//
+//  TabBarShowToggleApp.swift
+//  TabBarShowToggle
+//
+//  Created by Andres Felipe Ocampo Eljaiek on 20/02/2021.
+//
+
+import SwiftUI
+
+@main
+struct TabBarShowToggleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
